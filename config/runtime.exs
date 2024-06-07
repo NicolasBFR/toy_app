f649426bcc6d2fe1389f1d2a649992c4593d8c04
Nicolas Boganski <nicolas.boganski@epita.fr>
2024-06-07 20:22:17 +0200

    Setup Gigalixir deployment with Elixir Releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :toy_app, ToyApp.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
@@ -54,12 +54,9 @@ if config_env() == :prod do
   config :toy_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :toy_app, ToyAppWeb.Endpoint,
+    server: true,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
